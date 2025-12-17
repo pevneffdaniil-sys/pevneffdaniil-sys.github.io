02fcdf339d2c5007e1eddea6fea6d50e1f539586
pevneffdaniil-sys <pevneffdaniil@yandex.ru>
2025-12-17 06:24:15 +0300

    Практические

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1 практическая по JS
+function hello(name, age) {
+  return Привет, ${name}! Твой возраст: ${age};
+}
+console.log(hello("Йоруичи", 200));
+console.log(hello("Мисато", 29));
+console.log(hello("Фрирен", 1000));
